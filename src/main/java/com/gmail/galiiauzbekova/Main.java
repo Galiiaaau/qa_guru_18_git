f2ba80d23566908dbaa0df61787bfaf4e9fea51b
Galiia Uzbekova <galiiauzbekova@gmail.com>
2023-02-19 21:59:49 +0500

    added getMessage() method

--- src/main/java/com/gmail/galiiauzbekova/Main.java
+++ src/main/java/com/gmail/galiiauzbekova/Main.java
@@ -2,4 +2,8 @@ package com.gmail.galiiauzbekova;public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa_guru");
     }
+
+    private static String getMessage() {
+        return  "Hello qa_guru";
+    }
 }
